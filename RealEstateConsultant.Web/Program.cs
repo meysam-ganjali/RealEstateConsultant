9e0546d53c7c7ad0721313818ac06eeeee6bd2b7
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-12-09 08:03:53 -0800

    Add Identity To Project

--- RealEstateConsultant.Web/Program.cs
+++ RealEstateConsultant.Web/Program.cs
@@ -1,10 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RealEstateConsultant.Web.Data;
+
+#region Variables
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DataBaseContextConnection") ?? throw new InvalidOperationException("Connection string 'DataBaseContextConnection' not found.");
+
+
+#endregion
+
+#region Register Servises
+
+builder.Services.AddDbContext<DataBaseContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<DataBaseContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
+#endregion
+
+
+#region Configure the HTTP request pipeline
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -17,11 +39,16 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
+
+#endregion
+
+
