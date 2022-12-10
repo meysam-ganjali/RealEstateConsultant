cd8d30ff2a505f14888af82d64d3210a0dff3df7
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-12-10 07:19:02 -0800

    handle get Parent category

---
+++ RealEstateConsultant.Web/Areas/Admin/Controllers/MainCategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateConsultant.Application.UnitOfWorkPattern;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace RealEstateConsultant.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MainCategoryController : Controller
+    {
+        private readonly IHandleRepository _handleRepository;
+
+        public MainCategoryController(IHandleRepository handleRepository, IHostingEnvironment environment)
+        {
+            _handleRepository = handleRepository;
+            _environment = environment;
+        }
+        private IHostingEnvironment _environment;
+        public async Task<IActionResult> Index()
+        {
+            var res = await _handleRepository.MainCategory.GetAll(includeProperties: "ChildCategories");
+            return View(res);
+        }
+    }
+}
