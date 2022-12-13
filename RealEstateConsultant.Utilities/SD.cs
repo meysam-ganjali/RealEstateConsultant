163fbeb850cd78faf492530858f81c0acd857984
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-12-13 05:56:14 -0800

    Add Sd Class For Role Maneger

---
+++ RealEstateConsultant.Utilities/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstateConsultant.Utilities;
+
+public class SD
+{
+    public const string ManagerRole = "Manager";
+    public const string EmployeeRole = "Employee";
+    public const string CustomerRole = "Customer";
+}
