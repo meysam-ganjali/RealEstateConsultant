fbf03f6314b98c8f6d32e002016ed7bed0700f6f
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-12-14 05:38:12 -0800

    Handle Get Housing

---
+++ RealEstateConsultant.Web/Areas/Admin/Controllers/HousingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateConsultant.Application.UnitOfWorkPattern;
+using RealEstateConsultant.Utilities;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace RealEstateConsultant.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.ManagerRole)]
+    public class HousingController : Controller
+    {
+        #region ctor
+        private readonly IHandleRepository _handleRepository;
+        private IHostingEnvironment _environment;
+
+        public HousingController(IHandleRepository handleRepository, IHostingEnvironment environment)
+        {
+            _handleRepository = handleRepository;
+            _environment = environment;
+        }
+
+
+        #endregion
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _handleRepository.Housing.GetAll(
+                includeProperties: "ApplicationUser,HousingCategories,HousingProperties,HousingAmounts,HousingImages,HousingCategories.ChildCategory.MainCategory");
+            return View(model);
+        }
+    }
+}
